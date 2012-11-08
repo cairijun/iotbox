a746d84edb530f54cacc532caa5d4c62233c641a
Richard Tsai <dawning123@gmail.com>
2012-11-09 01:32:20 +0800

    解决中文乱码问题

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include <QApplication>
+#include <QTextCodec>
 #include "iotboxwidget.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    //网上找的解决乱码的代码
+    QTextCodec *codec = QTextCodec::codecForName("system");
+    QTextCodec::setCodecForTr(codec);
+    QTextCodec::setCodecForLocale(codec);
+    QTextCodec::setCodecForCStrings(codec);
+
     iotboxWidget w;
     w.show();
     
